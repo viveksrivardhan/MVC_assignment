d62c3fc8f5e7722f38c8b552da82ce8bf3bc6b95
Vivek Srivardhan <kv.viveksrivardhan@gmail.com>
2023-01-26 17:53:06 +0530

    minor modification

--- controllers/RegisterUser.js
+++ controllers/RegisterUser.js
@@ -1,14 +1,19 @@
 const {UserModel} = require("../models/UserSchema");
+const { generateHash,generateSalt} = require("../utils/encrypt");
 
 function RegisterUser(req, res) {
   let username = req.body.username;
   let email = req.body.email;
   let password = req.body.password;
+
+  const salt = generateSalt();
+  const hashedPassword = generateHash(password, salt);
+
   try{
   let userdata = UserModel.validate({
     username: username,
     email: email,
-    password: password,
+    password: hashedPassword,
   });
   let resultData = new UserModel(userdata);
   console.log(resultData)
